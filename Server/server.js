df2412b6fa424327b0c6ea1c5bf8db5bf951cb24
shalu1529 <144725677+shalu1529@users.noreply.github.com>
2025-04-11 18:44:39 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://blog-co8w.onrender.com/',
   credentials: true
 }));
 app.use(express.json());
